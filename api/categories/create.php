8f172629e2e0a11536b66400b20364824d55ae0b
Kira Selucky <kiraashley24@gmail.com>
2024-03-19 13:21:03 -0600

    fixed post to MUST need category filled out or receive error message

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,27 +1,30 @@
 <?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: POST');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-    // Include database and category files
-    include_once '../../config/Database.php';
-    include_once '../../models/Category.php';
+// Include necessary files
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
-    // Instantiate DB & connect
-    $database = new Database();
-    $db = $database->connect();
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
 
-    // Instantiate category object
-    $category = new Category($db);
+// Instantiate category object
+$category = new Category($db);
 
-    // Get raw posted data
-    $data = json_decode(file_get_contents("php://input"));
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
 
+// Check if category is provided
+if (!empty($data->category)) {
+    // Set category property
     $category->category = $data->category;
 
-    // Create Category
+    // Create category
     if ($category->create()) {
         echo json_encode(
             array('message' => 'Category Created')
@@ -31,3 +34,9 @@
             array('message' => 'Category Not Created')
         );
     }
+} else {
+    echo json_encode(
+        array('message' => 'Missing Required Parameters')
+    );
+}
+?>
