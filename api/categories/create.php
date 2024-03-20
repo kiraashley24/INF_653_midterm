bee338a341d5722fc3730401cb984a248a420cb4
Kira Selucky <kiraashley24@gmail.com>
2024-03-20 16:38:01 -0600

    copying based on author passing missed error

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,35 +1,42 @@
 <?php
-// Headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-// Include necessary files
-include_once '../../config/Database.php';
-include_once '../../models/Category.php';
+    // Include necessary files
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
 
-// Instantiate DB & connect
-$database = new Database();
-$db = $database->connect();
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
-// Instantiate category object
-$category = new Category($db);
+    // Instantiate category object
+    $category = new Category($db);
 
-// Get raw posted data
-$data = json_decode(file_get_contents("php://input"));
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
 
 
-$category->category = $data->category;
+    if (!empty($data->category)) {
+        // Set category property
+        $category->category = $data->category;
+        $result = $category->create();
 
-// Create category
-$result = $category->create();
-
-// Check if category was created successfully
-if (isset($result['id']) && isset($result['category'])) {
-    echo json_encode($result);
-} else {
-    echo json_encode($result);
-}
+        // Create category
+        if (isset($result['id']) && isset($result['category'])) {
+            echo json_encode($result);
+        } else {
+            echo json_encode(
+                array('message' => 'Author Not Created')
+            );
+        }
+    } else {
+        echo json_encode(
+            array('message' => 'Missing Required Parameters')
+        );
+    }
 
 ?>
