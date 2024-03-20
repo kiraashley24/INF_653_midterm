45f879e23c4220bb2d6c0059180f1987014d0c19
Kira Selucky <kiraashley24@gmail.com>
2024-03-20 12:06:46 -0600

    can't figure it out. going back to original

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,30 +1,51 @@
 <?php
-// Headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
-// Include necessary files
-include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
+    // Include necessary files
+    include_once '../../config/Database.php';
+    include_once '../../models/Quote.php';
 
-// Instantiate DB & connect
-$database = new Database();
-$db = $database->connect();
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
-// Instantiate quote object
-$quote = new Quote($db);
+    // Instantiate quote object
+    $quote = new Quote($db);
 
-// Quote read query
-$result = $quote->read();
+    // Quote read query
+    $result = $quote->read();
 
-// Check if any quotes
-if (!empty($result)) {
-    // Turn to JSON & output
-    echo json_encode($result);
-} else {
-    // No Quotes
-    echo json_encode(
-        array('message' => 'No Quotes Found')
-    );
-}
-?>
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any quotes
+    if ($num > 0) {
+        // Quote array
+        $quote_arr = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $quote_item = array(
+                'id' => $id,
+                'quote' => $quote,
+                'author_id' => $author_id,
+                'author' => $author_name,
+                'category_id' => $category_id,
+                'category' => $category_name
+            );
+
+            // Push to "data"
+            array_push($quote_arr, $quote_item);
+        }
+
+        // Turn to JSON & output
+        echo json_encode($quote_arr);
+    } else {
+        // No Quotes
+        echo json_encode(
+            array('message' => 'No Quotes Found')
+        );
+    }
