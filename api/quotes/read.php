c5487a76f0bb8ce2a1b3b3a7026068983fadd017
Kira Selucky <kiraashley24@gmail.com>
2024-03-20 12:03:33 -0600

    getting rowcount error

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,45 +1,30 @@
 <?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-    // Include necessary files
-    include_once '../../config/Database.php';
-    include_once '../../models/Quote.php';
+// Include necessary files
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
-    // Instantiate DB & connect
-    $database = new Database();
-    $db = $database->connect();
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
 
-    // Instantiate quote object
-    $quote = new Quote($db);
+// Instantiate quote object
+$quote = new Quote($db);
 
-    // Quote read query
-    $result = $quote->read();
+// Quote read query
+$result = $quote->read();
 
-    // Check if any quotes
-    if (!empty($result)) {
-        // Quote array
-        $quote_arr = array();
-
-        foreach ($result as $row) {
-            $quote_item = array(
-                'id' => $row['id'],
-                'quote' => $row['quote'],
-                'author' => $row['author'],
-                'category' => $row['category']
-            );
-
-            // Push to "data"
-            array_push($quote_arr, $quote_item);
-        }
-
-        // Turn to JSON & output
-        echo json_encode($quote_arr);
-    } else {
-        // No Quotes
-        echo json_encode(
-            array('message' => 'No Quotes Found')
-        );
-    }
+// Check if any quotes
+if (!empty($result)) {
+    // Turn to JSON & output
+    echo json_encode($result);
+} else {
+    // No Quotes
+    echo json_encode(
+        array('message' => 'No Quotes Found')
+    );
+}
 ?>
