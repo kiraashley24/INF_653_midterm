1850ed36d52fb6d4bc289999ec21c3cc15b2745d
Kira Selucky <kiraashley24@gmail.com>
2024-03-20 19:12:25 -0600

    working on category put requests

--- api/categories/update.php
+++ api/categories/update.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-// Include database and category files
+// Include necessary files
 include_once '../../config/Database.php';
 include_once '../../models/Category.php';
 
@@ -19,17 +19,23 @@ $category = new Category($db);
 // Get raw posted data
 $data = json_decode(file_get_contents("php://input"));
 
-// Set ID to update
-$category->id = $data->id;
-$category->category = $data->category;
+// Check if required fields are provided
+if (!empty($data->id) && !empty($data->category)) {
+    // Set category properties
+    $category->id = $data->id;
+    $category->category = $data->category;
 
-// Update category
-if ($category->update($data->category)) {
-    echo json_encode(
-        array('message' => 'Category Updated')
-    );
+    // Update category
+    $result = $category->update();
+
+    // Check if category was updated
+    if ($result) {
+        echo json_encode($result);
+    } else {
+        echo json_encode(array('message' => 'Category Not Updated'));
+    }
 } else {
-    echo json_encode(
-        array('message' => 'Category Not Updated')
-    );
+    echo json_encode(array('message' => 'Missing Required Parameters'));
 }
+
+?>
